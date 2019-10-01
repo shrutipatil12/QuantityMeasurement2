3e38e2580bb227a41fd2e74f3a64932dc6f07ade
shrutidhananjaypatil <shrutidpatil12@gmail.com>
2019-10-01 10:20:49 +0530

    [Shruti] Refactor. conversionToBase function by adding the baseUnit in it

--- src/main/java/com/thoughtworks/training/measurement/Unit.java
+++ src/main/java/com/thoughtworks/training/measurement/Unit.java
@@ -2,10 +2,10 @@ package com.thoughtworks.training.measurement;
 
 public enum Unit {
     INCH(1, Measurement.length),
-    FOOT(12, Measurement.length),
-    YARD(36, Measurement.length),
+    FOOT(12, Measurement.length, Unit.INCH),
+    YARD(36, Measurement.length, Unit.INCH),
     LITER(1, Measurement.volume),
-    GALLON(3.78, Measurement.volume);
+    GALLON(3.78, Measurement.volume, Unit.LITER);
 
     enum Measurement {
         length,
@@ -14,14 +14,22 @@ public enum Unit {
 
     private double conversionFactor;
     protected Measurement type;
+    private Unit baseUnit;
+
+    Unit(double conversionFactor, Measurement type, Unit baseUnit) {
+        this.conversionFactor = conversionFactor;
+        this.type = type;
+        this.baseUnit = baseUnit;
+    }
 
     Unit(double conversionFactor, Measurement type) {
         this.conversionFactor = conversionFactor;
         this.type = type;
-    }
 
-    double conversionToBase(double value) {
-        return value * conversionFactor;
+    }
+    Quantity conversionToBase(double value) {
+
+        return new Quantity(value * conversionFactor, baseUnit);
     }
 
 }
